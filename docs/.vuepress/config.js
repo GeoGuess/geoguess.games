c63d56f90c6da37111678f32919934e80b2fb7be
Bilel Jegham <contact.bileljegham@gmail.com>
2020-10-24 16:31:37 +0000

    fix(site): name

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -20,7 +20,7 @@ module.exports = {
   /**
    * Ref：https://v1.vuepress.vuejs.org/config/#title
    */
-  title: 'GeoGuess',
+  title: 'GeoGuess Beta',
   /**
    * Ref：https://v1.vuepress.vuejs.org/config/#description
    */
